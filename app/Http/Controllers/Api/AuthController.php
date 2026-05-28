559408fb54fc08216dbe3aa5a20b33a01065b767
mykhaylo zhovkevych <mzovkevic@gmail.com>
2026-05-28 18:34:11 +0200

    added errors printout. and controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,33 +6,32 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        // Validate data thanks the validated method
         $credentials = $request->validated();
 
         if (! Auth::attempt($credentials)) {
             return response([
-                'message' => 'Invalid Credentials'
+                'message' => 'Invalid Credentials',
             ], 401);
         }
+
         /** @var User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-
 
         return response(compact('user', 'token'));
     }
 
     public function signup(SignupRequest $request)
     {
-        // Validate data thanks the validated method
         $data = $request->validated();
+
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
@@ -46,9 +45,9 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        /** @var User $user*/
+        /** @var User $user */
         $user = $request->user();
-        $user->currentAccessToken()->delete();
+        $user?->currentAccessToken()?->delete();
 
         return response('', 204);
     }
